370228653372507deacf81261cc0494dabbbd47a
wangj28 <wangj28@mingyuanyun.com>
2017-11-10 17:40:50 +0800

    修复

--- controllers/login/index.js
+++ controllers/login/index.js
@@ -1,9 +1,9 @@
 module.exports = {
     'GET /': async (ctx, next) => {
-        ctx.response.body = '234234234';
+        ctx.response.body = ctx.session ? ctx.session.name : 'defautl';
     },
     'GET /signin': async (ctx, next) => {
-        ctx.session.name = "sdf";
+        ctx.session.name = "wangjing";
         ctx.response.body = "signin success";
     },
     'POST /signin': async (ctx, next) => {
