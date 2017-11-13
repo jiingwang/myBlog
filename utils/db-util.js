f91dbaa52966e354c9836c81011f0ede80f382ea
jiingwang <jingwang@novaszco.com>
2017-11-13 23:28:23 +0800

    新增models层

---
+++ utils/db-util.js
@@ -0,0 +1,47 @@
+const config = require('../config').db;
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'blog'
+});
+
+
+let query = function (sql, values) {
+	return new Promise((resolve, reject) => {
+		pool.getConnection(function (err, connection) {
+			if (err) {
+				reject(err);
+			} else {
+				connection.query(sql, values, function(err, rows) {
+					if(err) {
+						reject(err);
+					} else {
+						resolve(rows);
+					}
+				});
+
+				connection.release();
+			}
+		});
+	});
+};
+
+
+let createTable = (sql) => {
+	return query(sql, []);
+};
+
+let findDataById = (table, id) => {
+	let _sql = 'SELECT * FROM ?? WHERE ID = ?';
+	return query(_sql, [table, id]);
+};
+
+
+module.exports = {
+	query,
+	createTable,
+	findDataById
+}
